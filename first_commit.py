8d44c4c93fa3542fd42162b412a8e99dd427789c
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-06-18 00:03:24 +0200

    Add for loop

--- first_commit.py
+++ first_commit.py
@@ -2,3 +2,15 @@ dict = {
 "piekarnia": ["chleb", "pączek", "bułki"],
 "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+key_list = list(dict.keys())
+counter = 0
+print()
+
+for key in dict:
+  values = dict.get(key)
+  values_titled = []
+  for value in values:
+    values_titled.append(value.title())
+    counter += 1
+  print("Idę do", key.title(), "kupuję tu następujące rzeczy:", values_titled)
